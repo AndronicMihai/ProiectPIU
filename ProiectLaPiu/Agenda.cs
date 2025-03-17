b25d661ae072ef56a807bb2275d1b6b1259155bf
AndronicMihai <andronicmihaigabriel04@gmail.com>
2025-03-17 12:00:50 +0200

    Add files via upload

---
+++ ProiectLaPiu/Agenda.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AgendaPersonala.Models;
+
+namespace AgendaPersonala.Services
+{
+    public class Agenda
+    {
+        private List<Activitate> listaActivitati = new List<Activitate>();
+
+        public void AdaugaActivitate(Activitate activitate)
+        {
+            listaActivitati.Add(activitate);
+        }
+
+        public void StergeActivitate(int id)
+        {
+            listaActivitati.RemoveAll(a => a.Id == id);
+        }
+
+        public List<Activitate> AfiseazaActivitatiPeData(DateTime data)
+        {
+            return listaActivitati.Where(a => a.Data.Date == data.Date).ToList();
+        }
+
+        public List<Activitate> AfiseazaActivitatiRecurente(DateTime start, DateTime end)
+        {
+            return listaActivitati.Where(a => a.Recurenta && a.Data.Date >= start.Date && a.Data.Date <= end.Date).ToList();
+        }
+
+        public List<Activitate> AfiseazaActivitatiPeTip(string tip)
+        {
+            return listaActivitati.Where(a => a.Tip.Equals(tip, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+    }
+}
++
+    +
---
+++ ProiectLaPiu/ProiectLaPiu/Activitate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProiectLaPiu
+{
+    class Activitate
+    {
+        public int Id { get; set; }
+        public string Titlu { get; set; }
+        public string Descriere { get; set; }
+        public DateTime Data { get; set; }
+        public TimeSpan OraInceput { get; set; }
+        public TimeSpan OraSfarsit { get; set; }
+        public string Tip { get; set; } // Ex: "Munca", "Personal", "Sport"
+        public bool Recurenta { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}: {Titlu} ({Tip}) - {Data.ToShortDateString()} {OraInceput} - {OraSfarsit}";
+        }
+    }
+}
